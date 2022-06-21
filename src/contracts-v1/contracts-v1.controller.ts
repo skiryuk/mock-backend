41da8015454d4e07e6a9f0c728ac1d43d159ef2a
Oleg Skiryuk <OSkiryuk@beeline.ru>
2022-06-21 11:47:03 +0300

    DOL-1823 Изменение ввода ПДн для нерезидентов: добавлены методы и тогл фича

--- src/contracts-v1/contracts-v1.controller.ts
+++ src/contracts-v1/contracts-v1.controller.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 
 import * as GET_UNBLOCKED_ACRM_MOCK from './data/get-unblocked-acrm.json';
 import * as GET_FILTER_EXIST_CONTRACTS_MOCK from './data/get-filter-exists-contracts.json';
+import * as DOC_CONFIRM_MOCK from './data/doc-confirm.json';
 
 @Controller('v1/contract')
 export class ContractsV1Controller {
@@ -14,5 +15,10 @@ export class ContractsV1Controller {
   @Post('filter/exists')
   public async hasRejectedContracts(@Res() res: Response) {
     return res.status(HttpStatus.OK).json(GET_FILTER_EXIST_CONTRACTS_MOCK);
+  }
+
+  @Get('docconfirm')
+  public async docConfirm(@Res() res: Response) {
+    return res.status(HttpStatus.OK).json(DOC_CONFIRM_MOCK);
   }
 }
